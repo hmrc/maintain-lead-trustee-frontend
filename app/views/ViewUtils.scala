8a2a50ab170ee3818b909fa7c7d14dd2629eee81
carrielies <carrielies@users.noreply.github.com>
2021-07-16 10:43:17 +0100

    TRUS-4357 TidyUp

--- app/views/ViewUtils.scala
+++ app/views/ViewUtils.scala
@@ -54,8 +54,7 @@ object ViewUtils {
     error.toLowerCase.contains("date") || error.toLowerCase.contains("when")
   }
 
-  def mapRadioOptionsToRadioItems(field: Field, trackGa: Boolean,
-                                  inputs: Seq[RadioOption])(implicit messages: Messages): Seq[RadioItem] =
+  def mapRadioOptionsToRadioItems(field: Field, inputs: Seq[RadioOption])(implicit messages: Messages): Seq[RadioItem] =
     inputs.map(
       a => {
         RadioItem(
