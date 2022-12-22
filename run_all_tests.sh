2cf54c0af2d5a406cc77c7ce4ad16cac2160d9ae
Anusha Nagula <nagula.ann@gmail.com>
2022-12-22 00:34:28 +0000

    addressed comments

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sbt clean scalastyleAll compile coverage test it:test coverageOff coverageReport dependencyUpdate
+sbt clean scalastyleAll compile coverage test it:test coverageOff coverageReport dependencyUpdates
