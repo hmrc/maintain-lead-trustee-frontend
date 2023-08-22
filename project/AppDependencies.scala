0e35464e23996b87c2a300e6218cca1c69a25537
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2023-08-22 16:42:05 +0100

    DDCE-4194: Update CSP config, update deps, fix several warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,12 +1,11 @@
 import sbt._
 
 object AppDependencies {
-  import play.core.PlayVersion
   val bootstrapVersion = "7.21.0"
+  val mongoVersion = "1.3.0"
 
   val compile: Seq[ModuleID] = Seq(
-    play.sbt.PlayImport.ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "1.1.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % mongoVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.19.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion,
@@ -18,14 +17,11 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % bootstrapVersion,
     "org.scalatest"               %% "scalatest"                % "3.2.16",
-    "org.scalatestplus.play"      %% "scalatestplus-play"       % "5.1.0",
-    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % "1.1.0",
-    "org.scalatestplus"           %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
+    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % mongoVersion,
+    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.16.0",
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
-    "com.typesafe.play"           %% "play-test"                % PlayVersion.current,
-    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.14",
-    "org.scalacheck"              %% "scalacheck"               % "1.17.0",
-    "com.github.tomakehurst"      %  "wiremock-standalone"      % "2.27.2",
+    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.22",
+    "org.wiremock"                %  "wiremock-standalone"      % "3.0.1",
     "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % "it, test")
