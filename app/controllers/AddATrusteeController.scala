188428bccc3ab96a44c535ae8712cad7bdc7fb48
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2024-11-29 12:18:32 +0000

    DDCCE-3706 - Back Link Changes Have Been Fixed.

--- app/controllers/AddATrusteeController.scala
+++ app/controllers/AddATrusteeController.scala
@@ -67,7 +67,9 @@ class AddATrusteeController @Inject()(
         case all: AllTrustees =>
 
           val trustees = new AddATrusteeViewHelper(all).rows
-
+          println("=======================")
+          println(trustees)
+          println("=======================")
           if (all.size < 26) {
             Ok(addAnotherView(
               form = addAnotherForm,
