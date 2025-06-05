a013187d25df435881c7c495e00233427963fbfb
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2025-06-05 11:54:21 +0100

    DDCE-5360 reverting play frontend version due to failing tests

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,13 +1,13 @@
 import sbt.*
 
 object AppDependencies {
-  val bootstrapVersion = "9.13.0"
+g  val bootstrapVersion = "9.13.0"
   val mongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % mongoVersion,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "12.3.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "9.11.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"       %% "domain-play-30"                         % "11.0.0"
   )
