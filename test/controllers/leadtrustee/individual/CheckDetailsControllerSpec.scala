1f1122275d41a3ee44b3d70697fc8fd0635125aa
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-07-22 16:50:09 +0100

    DDCE-6773 - adding test cases

--- test/controllers/leadtrustee/individual/CheckDetailsControllerSpec.scala
+++ test/controllers/leadtrustee/individual/CheckDetailsControllerSpec.scala
@@ -270,6 +270,30 @@ class CheckDetailsControllerSpec extends SpecBase {
           status(result) mustEqual INTERNAL_SERVER_ERROR
         }
       }
+
+      "connector call" must {
+
+        "return InternalServerError" in {
+
+          val mockTrustConnector = Mockito.mock(classOf[TrustConnector])
+
+          val userAnswers = emptyUserAnswers
+
+          when(mockMapper.mapToLeadTrusteeIndividual(any())).thenReturn(Some(leadTrustee))
+          when(mockTrustConnector.amendLeadTrustee(any(), any())(any(), any())).thenReturn(Future.successful(HttpResponse(INTERNAL_SERVER_ERROR, "")))
+
+          val application = applicationBuilder(userAnswers = Some(userAnswers))
+            .overrides(
+              bind[TrustConnector].toInstance(mockTrustConnector),
+              bind[TrusteeMappers].toInstance(mockMapper)
+            ).build()
+
+          val request = FakeRequest(POST, onSubmitRoute.url)
+          val result = route(application, request).value
+
+          status(result) mustEqual INTERNAL_SERVER_ERROR
+        }
+      }
     }
   }
 }
